6540ce2b05f16782d1639969a6d17a3fb45f4762
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2019-03-13 14:22:48 -0400

    Creando Test

---
+++ SegundoParcial.Tests/UnitTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SegundoParcial.Tests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
